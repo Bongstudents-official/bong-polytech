f1cefc46fb9be6d3ad958f2d340cbc4bcbe9336b
Bongstudents-official <studentsbong@gmail.com>
2026-05-08 00:25:35 +0530

    Delete f.js

--- f.js
+++
@@ -1,9 +0,0 @@
-// changing css
-function darkmode(){
-    document.getElementById("body").style.background = "black";
-    document.getElementById("body").style.color = "white";
-}
-function brightmode(){
-    document.getElementById("body").style.background = "white";
-    document.getElementById("body").style.color = "black";
-}
