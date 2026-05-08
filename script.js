36480fa9c941f1dee55fc1740f03173f2816e33c
SHUBHADEEP SAMANTA <studentsbong@gmail.com>
2026-05-08 14:20:31 +0530

    Refactor and enhance quote display and search features

--- script.js
+++ script.js
@@ -54,3 +54,99 @@ function goSearch(event) {
 }
 
 //
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+        });
+    },
+
+    {
+        threshold: 0.15
+    }
+);
+
+reveals.forEach((section) => {
+    observer.observe(section);
+});
+//
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+    let count = 0;
+
+    const update = () => {
+        if (count < 1000) {
+            count += 25;
+
+            counter.innerText = count + "+";
+
+            setTimeout(update, 20);
+        }
+    };
+
+    update();
+});
+//
+
+const navMenu = document.getElementById("navMenu");
+
+document.addEventListener("click", function (event) {
+    const hamburger = document.querySelector(".hamburger");
+
+    if (!navMenu.contains(event.target) && !hamburger.contains(event.target)) {
+        navMenu.classList.remove("active");
+    }
+});
+//
+function addSubject() {
+    const row = document.createElement("div");
+
+    row.className = "subject-row";
+
+    row.innerHTML = `
+
+<input
+type="number"
+placeholder="Credits"
+class="credit">
+
+<input
+type="number"
+placeholder="Grade Point"
+class="grade">
+
+`;
+
+    document.getElementById("subjects").appendChild(row);
+}
+
+function calculateSGPA() {
+    const credits = document.querySelectorAll(".credit");
+
+    const grades = document.querySelectorAll(".grade");
+
+    let totalCredits = 0;
+
+    let totalPoints = 0;
+
+    for (let i = 0; i < credits.length; i++) {
+        const c = parseFloat(credits[i].value) || 0;
+
+        const g = parseFloat(grades[i].value) || 0;
+
+        totalCredits += c;
+
+        totalPoints += c * g;
+    }
+
+    const sgpa = totalPoints / totalCredits;
+
+    document.getElementById("result").innerText = "Your SGPA: " + sgpa.toFixed(2);
+}
